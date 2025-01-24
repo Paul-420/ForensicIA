6a2bc6e1fede41ea2da7807137d6b57967bd949c
paulj <pauljouvanceau@gmail.com>
2025-01-24 11:43:03 +0100

    make IA_testing read several data

--- IA_testing.py
+++ IA_testing.py
@@ -9,30 +9,28 @@ model = load('forensic.pkl')
 with open('columns.json', 'r') as f:
     columns = json.load(f)
 
-# Fonction pour détecter les mots-clés SQL
-def detect_sql_keywords(query):
-    sql_keywords = ["SELECT", "INSERT", "UPDATE", "DELETE", "DROP", "UNION", "ALTER", "CREATE", "EXEC", "XP_"]
-    return any(keyword in query.upper() for keyword in sql_keywords)
-
 # Lire le fichier JSON
 with open('test.json', 'r') as file:
     data = json.load(file)
 
-# Extraire les informations pertinentes
-method = data['method']
-url = data['url']
-query_parameters = '&'.join([f"{k}={v}" for k, v in data['query_parameters'].items()])
-headers = json.dumps(data['headers'])  # Convertir les headers en chaîne JSON
-#sql_keywords_detected = int(detect_sql_keywords(query_parameters))
+# Préparer les nouvelles données
+new_logs_list = []
+
+for entry in data:
+    method = entry['method']
+    url = entry['url']
+    query_parameters = '&'.join([f"{k}={v}" for k, v in entry['query_parameters'].items()])
+    headers = entry['headers']['User-Agent']  # Extraire uniquement le User-Agent
+    
+    new_logs_list.append({
+        'method': method,
+        'url': url,
+        'query_parameters': query_parameters,
+        'headers': headers
+    })
 
 # Créer un DataFrame avec les nouvelles données
-new_logs = pd.DataFrame({
-    'method': [method],
-    'url': [url],
-    'query_parameters': [query_parameters],
-    'headers': [headers],
-    #'sql_keywords_detected': [sql_keywords_detected]
-})
+new_logs = pd.DataFrame(new_logs_list)
 
 # Transformer les nouvelles données de la même manière que les données d'entraînement
 new_logs = pd.get_dummies(new_logs, columns=['method', 'url', 'query_parameters', 'headers'])
